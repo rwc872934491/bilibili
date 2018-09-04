74fb5a88ee18a294fcdcdd81b33d4a59a8d89232
Abizzz <1154893843@qq.com>
2018-09-04 16:51:31 +0800

    bilibili 1.2

--- src/main/java/org/lanqiao/dao/VideoInfoDaoImpl.java
+++ src/main/java/org/lanqiao/dao/VideoInfoDaoImpl.java
@@ -2,5 +2,23 @@ package org.lanqiao.dao;
 
 import org.lanqiao.entity.VideoInfo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VideoInfoDaoImpl extends BaseDao<VideoInfo> implements VideoInfoDao {
+    public List<VideoInfo> ShowVideoInfo(int pageNum, int pageSize){
+        return executeQuery("SELECT * FROM VideoInfo ORDER BY videoId LIMIT ?, ?",
+                new Object[]{(pageNum - 1) * pageSize, pageSize});
+    }
+
+    public int DeleteVideo(int videoId){
+        return executeUpdate("DELETE FROM VideoInfo WHERE videoId = ?",
+                new Object[]{videoId});
+    }
+
+    public static void main(String[] args){
+        VideoInfoDaoImpl videoInfoDao = new VideoInfoDaoImpl();
+        int ret = videoInfoDao.DeleteVideo(6);
+        System.out.println(ret);
+    }
 }
